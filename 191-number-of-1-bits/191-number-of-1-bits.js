decc19e08b2fdb104dc6825ed27e6cbe49e54a1f
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-06-02 14:04:03 -0400

    Time: 64 ms (94.73%), Space: 42.7 MB (39.32%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -4,8 +4,8 @@
  */
 var hammingWeight = function(n) {
     let count = 0;
-    while (n !==0) {
-        n = n & (n -1);
+    while (n !== 0) {
+        n = n & (n - 1);
         count++;
     }
     return count;
