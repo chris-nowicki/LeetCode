cd2fb85a20abb977e46013603777713ddf302036
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-10-20 11:45:35 -0400

    Time: 111 ms (37.31%), Space: 43.1 MB (20.49%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,28 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if (s.length % 2 !== 0) {
-        return false
-    }
-    
-    let stack = []
-    let closedToOpen = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
-    }
+	if (s.length % 2 !== 0) {
+		return false;
+	}
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] in closedToOpen) {
-            if (stack !== 0 && stack[stack.length - 1] == closedToOpen[s[i]]) {
-                stack.pop()
-            } else {
+	let stack = [];
+	let closedToOpen = new Map([
+		[")", "("],
+		["]", "["],
+		["}", "{"],
+	]);
+
+	for (let i = 0; i < s.length; i++) {
+		if (closedToOpen.has(s[i])) {
+			if (stack !== 0 && stack[stack.length - 1] == closedToOpen.get(s[i])) {
+				stack.pop();
+			} else {
                 return false
             }
-        } else {
-            stack.push(s[i])
-        }
-    }
+		} else {
+			stack.push(s[i]);
+		}
+	}
 
-    return stack.length === 0
+	return stack.length === 0;
 };
