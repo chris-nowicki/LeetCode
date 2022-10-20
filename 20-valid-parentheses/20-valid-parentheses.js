99459d58afd20fbcf62e7a6953a957e4fb4d667c
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-10-20 11:37:58 -0400

    Time: 108 ms (42.96%), Space: 42.7 MB (46.84%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if (s.length % 2 !== 0) {
+        return false
+    }
+    
+    let stack = []
+    let closedToOpen = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in closedToOpen) {
+            if (stack !== 0 && stack[stack.length - 1] == closedToOpen[s[i]]) {
+                stack.pop()
+            } else {
+                return false
+            }
+        } else {
+            stack.push(s[i])
+        }
+    }
+
+    return stack.length === 0
+};
